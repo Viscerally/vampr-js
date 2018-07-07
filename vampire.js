c84be5d65b731f6ce8fbabeccbd51c5f08856a28
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-07-06 17:47:23 -0700

    suck it, Twighlight

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentCreator = this;
+    while (currentCreator.creator) {
+      currentCreator = currentCreator.creator;
+      numberOfVamps++;
+    }
 
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.offspring.length > vampire.offspring.length) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
